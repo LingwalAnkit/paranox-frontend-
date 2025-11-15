60af1bdafd9957ad8e67caece09e4073aec7b3bb
Ankit Lingwal <ankitlingwal9@gmail.com>
2025-11-16 03:40:05 +0530

    made chnages

--- src/app/(teacher)/teacher/page.tsx
+++ src/app/(teacher)/teacher/page.tsx
@@ -1,12 +1,18 @@
-"use client"
-import React from "react"
-import SidebarComponent from '../../component/sidebar/teacherSidebar'
-import {BentoGridDemo} from '@/component/acertinity/WobbleCardTeacher'
+"use client";
+import React from "react";
+import SidebarComponent from "../../../component/sidebar/teacherSidebar";
+import { BentoGridDemo } from "@/component/acertinity/WobbleCardTeacher";
 
 const Home = () => {
   return (
-    <SidebarComponent components={<div className='flex align-middle '><BentoGridDemo /></div>}/>
-  )
-}
+    <SidebarComponent
+      components={
+        <div className="flex align-middle ">
+          <BentoGridDemo />
+        </div>
+      }
+    />
+  );
+};
 
-export default Home
+export default Home;
